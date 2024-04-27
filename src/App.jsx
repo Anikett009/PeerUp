b4931db949e748a834bf44650ffeb3410cbd5a54
Tanush Salian <tanush1852@gmail.com>
2024-04-28 04:01:09 +0530

    tanush commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-export default function App() {
+import React from "react";
+import Signup from "./components/Signup"
+import "./index.css"
+import Home from "./components/Home.jsx"
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="overflow-x-hidden">
+      <Home/>
+      </div>
+
   )
 }
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
-  plugins: [],
+  plugins: [
+    function({ addUtilities }) {
+      const newUtilities = {
+        '.select-none': {
+          '-webkit-touch-callout': 'none',
+          '-webkit-user-select': 'none',
+          '-moz-user-select': 'none',
+          '-ms-user-select': 'none',
+          'user-select': 'none',
+        },
+        '.select-all': {
+          '-webkit-touch-callout': 'auto',
+          '-webkit-user-select': 'all',
+          '-moz-user-select': 'all',
+          '-ms-user-select': 'all',
+          'user-select': 'all',
+        },
+      };
+
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
 }
+
